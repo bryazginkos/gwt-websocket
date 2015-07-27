e4b08e50c3f1a13d9b355f82c2e813ec072b7e49
bryazginkos <bryazginkos@yahoo.com>
2015-07-27 22:13:02 +0600

    merge fix

--- gwt-websocket-client/src/main/java/ru/gwtwebsocket/ServletInitializer.java
+++
@@ -1,7 +0,0 @@
-package ru.gwtwebsocket;
-
-/**
- * Created by Константин on 23.07.2015.
- */
-public class ServletInitializer {
-}
--- gwt-websocket-client/src/main/java/ru/gwtwebsocket/config/WebSocketConfig.java
+++
@@ -1,7 +0,0 @@
-package ru.gwtwebsocket.config;
-
-/**
- * Created by Константин on 23.07.2015.
- */
-public class WebSocketConfig {
-}
