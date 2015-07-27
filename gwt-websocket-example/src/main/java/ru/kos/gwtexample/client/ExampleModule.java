064adaca556941125bc6d4c4b90843f5c7368176
bryazginkos <bryazginkos@yahoo.com>
2015-07-27 22:02:45 +0600

    merge fix

--- gwt-websocket-example/src/main/java/ru/kos/gwtexample/client/ExampleModule.java
+++ gwt-websocket-example/src/main/java/ru/kos/gwtexample/client/ExampleModule.java
@@ -18,6 +18,40 @@ import ru.gwtwebsocket.dto.client.ServerInfo;
  */
 public class ExampleModule implements EntryPoint {
     public void onModuleLoad() {
+        Button buttonConn = new Button("Connect");
+        Button buttonSend = new Button("Send");
 
+        WSConfiguration<ClientInfo, ServerInfo> wsConfiguration = new WSConfiguration<ClientInfo, ServerInfo>()
+                .withUrl("http://127.0.0.1:8080/websocketservice")
+                .withSubscribeUrl("/topic/info")
+                .withGClass(ServerInfo.class)
+                .withSClass(ClientInfo.class)
+                .withAutoBeanFactory(GWT.<DtoFactory>create(DtoFactory.class))
+                .withCallback(new WSCallback<ServerInfo>() {
+                    @Override
+                    public void onMessage(ServerInfo serverInfo) {
+                        String msg = serverInfo.getProblem() + "=" + serverInfo.getResult();
+                        msg += " " + serverInfo.getServerAddInfo().getTime();
+                        Window.alert(msg);
+                    }
+                });
+
+        final WSComponent<ClientInfo, ServerInfo> wsComponent = new WSComponent<>(wsConfiguration);
+        RootPanel.get().add(buttonConn);
+        RootPanel.get().add(buttonSend);
+
+        buttonConn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                wsComponent.connect();
+            }
+        });
+
+        buttonSend.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                wsComponent.send(new ClientInfoImpl(3,2));
+            }
+        });
     }
 }
